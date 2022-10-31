f6f24e70c51950faf346b4161fa0b1b1773fbb3d
Panmaeja <mbonadiman2014@gmail.com>
2022-10-31 15:30:57 -0300

    ATT INDEX

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -8,10 +8,8 @@ $this->title = 'Doa+';
 
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Doa+</h1>
-
-        <p class="lead">Seja bem-vindo ao maior site de doações de chapecó!</p>
-
+       
+        <img src="../img/logo.PNG">
         <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 750px">
         <iframe src="https://www.google.com/maps/d/embed?mid=1bFo2rspDg_a0WZt6Hvn4oORZAth6qwc&ehbc=2E312F" width="100%" height="100%"></iframe>
         </div>
