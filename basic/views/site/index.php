a0d0b63227ea22250ab84a59edc637c9789239ee
Panmaeja <mbonadiman2014@gmail.com>
2022-10-31 14:50:34 -0300

    mapa 2

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -6,21 +6,14 @@ $this->title = 'Doa+';
 ?>
 <div class="site-index">
 
+
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Doa+</h1>
 
         <p class="lead">Seja bem-vindo ao maior site de doações do oeste catarinense!</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-            <iframe src="https://www.google.com/maps/d/embed?mid=1bFo2rspDg_a0WZt6Hvn4oORZAth6qwc&ehbc=2E312F" width="80%" height="80%"></iframe>
-            </div>
-        </div>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1bFo2rspDg_a0WZt6Hvn4oORZAth6qwc&ehbc=2E312F" width="100%" height="100%"></iframe>
+       
 
     </div>
 </div>
