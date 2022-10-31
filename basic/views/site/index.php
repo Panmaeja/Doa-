3632b9cbeb1f190d18b995287bebe16b4f682c12
Panmaeja <mbonadiman2014@gmail.com>
2022-10-31 15:00:00 -0300

    mapa att 3

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -12,7 +12,9 @@ $this->title = 'Doa+';
 
         <p class="lead">Seja bem-vindo ao maior site de doações do oeste catarinense!</p>
 
+        <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
         <iframe src="https://www.google.com/maps/d/embed?mid=1bFo2rspDg_a0WZt6Hvn4oORZAth6qwc&ehbc=2E312F" width="100%" height="100%"></iframe>
+        </div>
        
 
     </div>
