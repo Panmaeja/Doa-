1d5e3803bfefc4ef6563c6ac68d5ef4d10922405
Panmaeja <mbonadiman2014@gmail.com>
2022-11-21 17:04:12 -0300

    ibagens habilton

--- basic/views/donatarios/_form.php
+++ basic/views/donatarios/_form.php
@@ -7,7 +7,8 @@ use yii\widgets\ActiveForm;
 /** @var app\models\Donatarios $model */
 /** @var yii\widgets\ActiveForm $form */
 ?>
-<section class="vh-100" style="background-color: #eee;">
+
+
 <div class="container h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-lg-12 col-xl-11">
@@ -15,28 +16,25 @@ use yii\widgets\ActiveForm;
           <div class="card-body p-md-5">
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-              <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Cadastre-se no Doa+</p>
-              <form class="mx-1 mx-md-4">
-
+              <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Cadastrar-se no Doa+</p>
 
 <div class="donatarios-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
     <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
     <?= $form->field($model, 'nome')->textInput(['maxlength, placeholder="Nome completo"' => true]) ?>
     </div>
     </div>
-
+    
     <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
     <?= $form->field($model, 'cpfcnpj')->textInput(['maxlength, placeholder="xxx.xxx.xxx-xx"' => true]) ?>
     </div>
     </div>
-
+     
     <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
@@ -57,26 +55,24 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'email')->textInput(['maxlength, placeholder="exemplo@gmail.com"' => true]) ?>
     </div>
     </div>
-
+     
     <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
     <?=$form->field($model, 'senha')->textInput(['maxlength' => true]) ?>
     </div>
     </div>
- 
+   
     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
         <?= Html::submitButton('Save', ['class' => 'btn btn-primary btn-lg']) ?>
-    </div>
-    </form>
-    </div>
-            </div>
+    
+<?php ActiveForm::end(); ?>
+
+</div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 </div>
-</section>
-<?php ActiveForm::end(); ?>
+</div>
